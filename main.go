fa6e3ceb91bccdbdffd145dd4e22ec7eb320fe77
Ming-der Wang <ming@log4analytics.com>
2015-08-16 09:43:03 +0800

    ok for parsing comments

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"go/ast"
 	"go/parser"
 	"go/token"
 )
@@ -11,23 +13,38 @@ type Slack struct {
 }
 
 var src = `
+//go:generate ginger $GOFILE
 package main
+// @gigner
 type Slack struct {
-	Name string
+	Name string 
 }
 func main() {
-	println("Hello World")
+	println("hello ginger")
 }
 `
 
 func main() {
 	fset := token.NewFileSet()
-	f, err := parser.ParseFile(fset, "", src, 0)
+	f, err := parser.ParseFile(fset, "", src, parser.ParseComments)
 	if err != nil {
 		panic(err)
 	}
-	//ast.Print(fset, f)
-	for _, decl := range f.Decls {
-		fmt.Println(decl)
+	ast.Print(fset, f)
+	fmt.Println("-----------------")
+	for index, decl := range f.Decls {
+		fmt.Println(index)
+		spew.Dump(decl)
+		genDecl, ok := decl.(*ast.GenDecl)
+		if !ok {
+			fmt.Println("!ok")
+		} else {
+			if genDecl.Doc == nil {
+				fmt.Println("genDecl.Doc == nil")
+			} else {
+				spew.Dump(genDecl.Doc)
+
+			}
+		}
 	}
 }
